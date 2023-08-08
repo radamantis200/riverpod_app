a44462a6dc7dc8628f822101bbd1ddcb635216f6
Jefry Sanchez <sanchezmorajeffrey@gmail.com>
2023-08-07 19:33:02 -0600

    add file screens add screen home_screen

---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: _HomeScreenView()),
+    );
+  }
+}
+
+class _HomeScreenView extends StatelessWidget {
+  const _HomeScreenView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        _CustomListTile(
+            title: 'Provider Simple',
+            subTitle: 'Un provider de sólo lectura',
+            location: '/provider'),
+      ],
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  final String title;
+  final String subTitle;
+  final String location;
+
+  const _CustomListTile({
+    required this.title,
+    required this.subTitle,
+    required this.location,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(subTitle),
+      trailing: const Icon(Icons.arrow_forward_ios_rounded),
+      onTap: () => context.push(location),
+    );
+  }
+}
---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:flutter_riverpod_app/presentation/screens/home_screen.dart';
+export 'package:flutter_riverpod_app/presentation/screens/01_provider/provider_screen.dart';
